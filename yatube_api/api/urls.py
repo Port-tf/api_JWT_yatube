be15fac83f24714ed8d0ca087c0c4991cd2d79ee
Игорь Шкода <port_tf@bk.ru>
2022-07-21 12:38:24 +0300

    Sprint 9.4

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -12,7 +12,7 @@ v1_router.register(
 v1_router.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
-
+    path('v1/', include('djoser.urls')),
     path('v1/', include('djoser.urls.jwt')),
     path('v1/', include(v1_router.urls)),
 ]
